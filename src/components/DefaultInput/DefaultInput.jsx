92a0197e07442ad87f1c4b37b3cc24dc3d624921
Alan Paiva <allanpaiva21@outlook.com>
2022-03-28 13:54:26 -0300

    Added React Hook Form

---
+++ src/components/DefaultInput/DefaultInput.jsx
@@ -0,0 +1,10 @@
+const DefaultInput = ({ name, onChange, value }) => {
+  console.log('aaa');
+  return (
+    <div>
+      <input name={name} onChange={onChange} value={value} />
+    </div>
+  );
+};
+
+export default DefaultInput;
